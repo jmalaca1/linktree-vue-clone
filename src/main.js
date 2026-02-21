7e9e6b39bcb6c1df5896ca7491db160f150239a9
Jacqueline MalacaraOrtega <jmalaca1@stu.southtexascollege.edu>
2026-02-20 20:35:01 -0600

    ..

--- src/main.js
+++ src/main.js
@@ -3,6 +3,5 @@ import '@/assets/style.css'
 import router from '@/router'
 import App from './App.vue'
 
-console.log('Main.js is running')
 
 createApp(App).use(router).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  server: {
-    port: 5173,
-    open: true,
-    host: true,
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
