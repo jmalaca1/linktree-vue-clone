4b26d5556b643b084935bb22f0798af741595bfc
Jacqueline MalacaraOrtega <jmalaca1@stu.southtexascollege.edu>
2026-02-21 00:07:32 -0600

    lastly

--- src/components/LinkCard.vue
+++ src/components/LinkCard.vue
@@ -3,14 +3,14 @@
   href="#" 
   target="_blank" 
   rel="noopener noreferrer" 
-  class="flex w-full items-center gap-4 rounded-xl border border-gray-700 bg-gray-300 p-4 shadow-md shadow-gray-500/50 hover:scale-[1.02] hover:shadow-xl hover:border-blue-500 hover:shadow-blue-400/50 transition-all"
+  class="group relative flex w-full items-center gap-4 rounded-xl border border-gray-700 bg-gray-300 p-4 shadow-md shadow-gray-500/50 hover:scale-[1.02] hover:shadow-xl hover:border-blue-500 hover:shadow-blue-400/50 transition-all duration-300"
   >
   <!-- Icon Container -->
-    <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-pink-400 text-purple-600">
+    <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-pink-400 text-purple-600 transition-all duration-300 group-hover:scale-110 group-hover:bg-blue-400 group-hover:text-white group-hover:shadow-xl group-hover:shadow-purple-500/50">
       <svg viewBox="0 0 24 24" 
       fill="none" 
       stroke="currentColor" 
-      class="h-6 w-6"
+      class="h-6 w-6 transition-transform duration-300 group-hover:rotate-6"
       >
       <path 
       stroke-linecap="round" 
@@ -21,10 +21,14 @@
       </svg>
     </div>
 
-    <!-- Text Content -->
+  <!-- Text Content -->
     <div class="min-w-0 flex-1">
       <h3 class="font-display truncate font-semibold text-gray-600">Link Title</h3>
       <p class="truncate text-sm text-gray-500">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>      
     </div>
+
+  <!-- Hover Glow Effect -->
+    <div class="absolute inset-0 rounded-xl bg-linear-to-r from-blue-400/0 via-blue-400/20 to-blue-400/0 opacity-0 transition-opacity duration-100 group-hover:opacity-100"/>
+
   </a>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-
 import vue from '@vitejs/plugin-vue'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 import tailwindcss from '@tailwindcss/vite'
-
-import { cloudflare } from '@cloudflare/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
