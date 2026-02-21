198fc8ec2d8524d57ad95bcc282a9c48d536d508
Jacqueline MalacaraOrtega <jmalaca1@stu.southtexascollege.edu>
2026-02-20 21:18:31 -0600

    com7

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,21 @@
+<script>
+import { ref } from 'vue'
+import LinkCard from '@/components/LinkCard.vue';
+
+const profile = ref({
+  name: 'Jacqueline Malacara Ortega',
+  description: 'Program Developer and Student',
+  image: '"C:\\Users\\adan\\Downloads\\IMG_0024.jpg"',
+});
+</script>
+
 <template>
-  <main class="flex flex-1 items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">Hello There</h1>
+  <main class="flex min-h-screen flex-col items-center px-4 py-8">
+    <!--Profile Header-->
+    <div>
+      <img :src="profile.image" :alt="profile.name">
+      <h1>{{ profile.name }}</h1>
+      <p>{{ profile.description }}</p>
+    </div>
   </main>
 </template>
